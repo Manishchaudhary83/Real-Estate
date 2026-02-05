10531fdd2ae8207df52656bf4c58dce68d699a77
Manish Chaudhary <manishchaudhary@Manishs-MacBook-Air.local>
2026-02-05 09:26:10 +0545

    Fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react';
 import '@n8n/chat/style.css';
 import { createChat } from '@n8n/chat';
-
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
-import Testimonials from './components/TEstimonials';
+import Testimonials from "./components/Testimonials";
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
