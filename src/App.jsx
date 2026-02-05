a1d9b584fe8453d0a1c7db9c8be2fe8e1a05b673
Manish Chaudhary <manishchaudhary@Manishs-MacBook-Air.local>
2026-02-05 09:12:30 +0545

    Fix case-sensitive import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
 import React, { useEffect } from 'react';
-import Header from './components/Header';
 import '@n8n/chat/style.css';
 import { createChat } from '@n8n/chat';
+
+import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Testimonials from './components/TEstimonials';
 import Contact from './components/Contact';
-import { ToastContainer} from 'react-toastify';
 import Footer from './components/Footer';
-
+import { ToastContainer } from 'react-toastify';
 
 function App() {
+   useEffect(() => {
+  createChat({
+    webhookUrl: 'https://manishchaudhary.app.n8n.cloud/home/chat/67c00d94-260c-4c68-9e0c-89565f680811',
+  });
+}, []);
 
-  useEffect(() => {
-    createChat({
-      webhookUrl: 'https://manishchaudhary.app.n8n.cloud/webhook/654362d6-2a60-4695-b9b0-cf085180c4d3/chat',
-    });
-  }, []);
 
   return (
     <div className="w-full overflow-hidden">
-      
-      <ToastContainer/>
+      <ToastContainer />
       <Header />
-      <About/>
-      <Projects/>
-      <Testimonials/>
-      <Contact/>
-       <Footer/>
-   
-        
+      <About />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
